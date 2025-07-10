626cc1450eae6a8eef776c37e5a73c6cc3e5e525
Muneer Hamdan <muneerhamdan01@gmail.com>
2025-07-09 23:04:06 -0400

    floating window

--- piano.sh
+++ piano.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-#xfce4-terminal -e /home/muneer/piano-c/pianosdl
-xfce4-terminal --working-directory=/home/muneer/piano-c -e "make clean" -e "make" -e "make run"
+xfce4-terminal --title="piano" --working-directory=/home/muneer/piano-c -e "make clean" -e "make" -e "make run"
+
+# Give it time to spawn
+sleep 0.2
+
+# Make it floating, center it, and focus it
+i3-msg '[title="piano"] floating enable, move position center, focus'
+
